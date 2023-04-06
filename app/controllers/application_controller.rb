5a97a03f2a3ee4076b320848ace439305dd1928b
Nkuria <nkuria01@gmail.com>
2023-04-06 08:36:59 +0300

    Worked on authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  include Authenticable
+  before_action :authorize_request
+
+  def render_error(message, status_code)
+    render json: {
+      error: {
+        message:,
+        code: status_code
+      }
+    }, status: status_code
+  end
 end
