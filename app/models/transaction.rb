f1072f35d61471f363f8274e4c300b79aa179ccb
Nkuria <nkuria01@gmail.com>
2023-04-06 12:44:46 +0300

    Worked on transaction model test

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,8 @@
 class Transaction < ApplicationRecord
-    belongs_to :sender, class_name: 'User'
-    belongs_to :receiver, class_name: 'User'
+  belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
+  belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
+
+  validates :amount, :status, presence: true
 end
 
 # == Schema Information
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'associations' do
+    it { should belong_to(:sender).class_name('User').with_foreign_key('sender_id') }
+    it { should belong_to(:receiver).class_name('User').with_foreign_key('receiver_id') }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:status) }
+    it { should validate_presence_of(:amount) }
+  end
 end
 
 # == Schema Information
